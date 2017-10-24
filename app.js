c0e3edeb26a5d3be26f3e5dc671e7709bd04d228
INDEZENDE <i.rios.zendejas@gmail.com>
2017-10-24 15:19:12 -0500

    archivos completos

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
 
-console.log('hola');
+// agregar imagen![leyenda ](ruta)
+var comparacion=function (array1, array2){
+  if (array1.length !== array2.length)
+  {  document.write("los arreglos son diferentes");
+    return false;  }
+
+  else {
+    for (var i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i])
+
+      {  document.write("los arreglos son diferentes");
+        return false;}
+    }
+    document.write("los arreglos son iguales");
+    return true;
+  }
+}
+var arreglo1=[1,2,3,4,5,6,7,8,9,0];
+var arreglo2=[1,2,3,4,5,6,7,8,9,0];
+
+console.log(comparacion(arreglo1,arreglo2));
